44cc1591e2703a2b471eaf9d67f5381a4aac9260
cristianpmorales <39247282+cristianpmorales@users.noreply.github.com>
2018-07-04 23:56:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+//The output should be two capital letters with a dot separating them.
+
+function abbrevName(name){
+  var nam = name.split(' ');
+  return nam[0].charAt(0).toUpperCase()+'.' + nam[1].charAt(0).toUpperCase();
+
+}
